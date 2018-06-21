2b863860b5c3b174220e237943ce693d59da47ac
PepsiDog <pepsidawg00@gmail.com>
2018-06-21 14:56:39 -0400

    Add ItemBuilder

--- src/main/java/io/github/mrsperry/mcutils/ItemBuilder.java
+++ src/main/java/io/github/mrsperry/mcutils/ItemBuilder.java
@@ -1,4 +1,65 @@
 package io.github.mrsperry.mcutils;
 
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ItemBuilder {
+    private ItemStack item;
+    private ItemMeta meta;
+
+    public ItemBuilder() {
+        this(Material.STONE);
+    }
+
+    public ItemBuilder(Material material) {
+        this.item = new ItemStack(material, 1);
+        this.meta = this.item.getItemMeta();
+        this.meta.setLore(new ArrayList<String>());
+    }
+
+    public ItemBuilder material(Material material) {
+        this.item.setType(material);
+        return this;
+    }
+
+    public ItemBuilder amount(int amount) {
+        this.item.setAmount(amount);
+        return this;
+    }
+
+    public ItemBuilder data(short data) {
+        this.item.setDurability(data);
+        return this;
+    }
+
+    public ItemBuilder name(String name) {
+        this.meta.setDisplayName(name);
+        return this;
+    }
+
+    public ItemBuilder lore(List<String> lore) {
+        this.meta.setLore(lore);
+        return this;
+    }
+
+    public ItemBuilder addLore(String loreLine) {
+        List<String> temp = this.meta.getLore();
+        temp.add(loreLine);
+        return this.lore(temp);
+    }
+
+    public ItemBuilder enchant(Enchantment enchantment, int level) {
+        this.meta.addEnchant(enchantment, level, true);
+        return this;
+    }
+
+    public ItemStack build() {
+        this.item.setItemMeta(this.meta);
+        return this.item;
+    }
 }

cfb202369c8ee2f843f7d6e746dffe17c018ff3a
Josh <mrjoshuasperry@gmail.com>
2018-06-21 14:56:31 -0400

    Add message formatter

--- src/main/java/io/github/mrsperry/mcutils/MessageFormatter.java
+++ src/main/java/io/github/mrsperry/mcutils/MessageFormatter.java
@@ -1,15 +1,20 @@
 package io.github.mrsperry.mcutils;
 
+import net.md_5.bungee.api.ChatColor;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class MessageFormatter {
     public static String addTimestamp() {
-
+        return ChatColor.DARK_GRAY + "[" + ChatColor.GRAY + new SimpleDateFormat("HH:mm").format(Calendar.getInstance().getTime()) + ChatColor.DARK_GRAY + "]";
     }
 
     public static String addWorldName(String name) {
-
+        return ChatColor.DARK_GRAY + "[" + ChatColor.GRAY + name + ChatColor.DARK_GRAY + "]";
     }
 
-    public static String addTimeAndName(String name) {
-
+    public static String addTimeAndWorld(String name) {
+        return addTimestamp() + addWorldName(name);
     }
 }

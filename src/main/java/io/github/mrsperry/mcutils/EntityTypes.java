ad5637df45a597bead67ea693ab60ab6bc738c0b
Josh <mrjoshuasperry@gmail.com>
2018-08-13 02:01:32 -0400

    Add skeleton type to hostile mob list (oops)

--- src/main/java/io/github/mrsperry/mcutils/EntityTypes.java
+++ src/main/java/io/github/mrsperry/mcutils/EntityTypes.java
@@ -7,24 +7,24 @@ import java.util.ArrayList;
 
 public class EntityTypes {
     private static ArrayList<EntityType> hostile = Lists.newArrayList(
-            EntityType.BLAZE,           EntityType.CAVE_SPIDER,  EntityType.CREEPER,
-            EntityType.ELDER_GUARDIAN,  EntityType.ENDER_DRAGON, EntityType.ENDERMAN,
-            EntityType.EVOKER,          EntityType.GHAST,        EntityType.GIANT,
-            EntityType.GUARDIAN,        EntityType.HUSK,         EntityType.ILLUSIONER,
-            EntityType.MAGMA_CUBE,      EntityType.PIG_ZOMBIE,   EntityType.SHULKER,
-            EntityType.SILVERFISH,      EntityType.SILVERFISH,   EntityType.SLIME,
-            EntityType.SPIDER,          EntityType.STRAY,        EntityType.VEX,
-            EntityType.VINDICATOR,      EntityType.WITCH,        EntityType.WITHER,
-            EntityType.WITHER_SKELETON, EntityType.ZOMBIE
+            EntityType.BLAZE,          EntityType.CAVE_SPIDER,     EntityType.CREEPER,
+            EntityType.ELDER_GUARDIAN, EntityType.ENDER_DRAGON,    EntityType.ENDERMAN,
+            EntityType.EVOKER,         EntityType.GHAST,           EntityType.GIANT,
+            EntityType.GUARDIAN,       EntityType.HUSK,            EntityType.ILLUSIONER,
+            EntityType.MAGMA_CUBE,     EntityType.PIG_ZOMBIE,      EntityType.SHULKER,
+            EntityType.SILVERFISH,     EntityType.SILVERFISH,      EntityType.SKELETON,
+            EntityType.SLIME,          EntityType.SPIDER,          EntityType.STRAY,
+            EntityType.VEX,            EntityType.VINDICATOR,      EntityType.WITCH,
+            EntityType.WITHER,         EntityType.WITHER_SKELETON, EntityType.ZOMBIE
     );
 
     private static ArrayList<EntityType> neutral = Lists.newArrayList(
-            EntityType.BAT,             EntityType.COW,          EntityType.CHICKEN,
-            EntityType.DONKEY,          EntityType.HORSE,        EntityType.IRON_GOLEM,
-            EntityType.LLAMA,           EntityType.MULE,         EntityType.MUSHROOM_COW,
-            EntityType.OCELOT,          EntityType.PARROT,       EntityType.PIG,
-            EntityType.POLAR_BEAR,      EntityType.RABBIT,       EntityType.SHEEP,
-            EntityType.SNOWMAN,         EntityType.SQUID
+            EntityType.BAT,            EntityType.COW,             EntityType.CHICKEN,
+            EntityType.DONKEY,         EntityType.HORSE,           EntityType.IRON_GOLEM,
+            EntityType.LLAMA,          EntityType.MULE,            EntityType.MUSHROOM_COW,
+            EntityType.OCELOT,         EntityType.PARROT,          EntityType.PIG,
+            EntityType.POLAR_BEAR,     EntityType.RABBIT,          EntityType.SHEEP,
+            EntityType.SNOWMAN,        EntityType.SQUID
     );
 
     public static ArrayList<EntityType> getAllTypes() {

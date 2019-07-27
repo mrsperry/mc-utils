e36f183d64ba498ef8e7a74f0b217a1af639e72b
Josh <mrjoshuasperry@gmail.com>
2019-07-26 21:13:00 -0400

    Add a decor menu item that does nothing when clicked

---
+++ src/main/java/io/github/mrsperry/mcutils/menu/items/DecorMenuItem.java
@@ -0,0 +1,9 @@
+package io.github.mrsperry.mcutils.menu.items;
+
+import org.bukkit.inventory.ItemStack;
+
+public class DecorMenuItem extends MenuItem {
+    public DecorMenuItem(int slot, ItemStack item) {
+        super(slot, item, null);
+    }
+}

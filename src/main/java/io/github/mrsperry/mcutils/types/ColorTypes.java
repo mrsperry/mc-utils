805389002b5129f2185582f7fdf6a270acea35df
Josh <mrjoshuasperry@gmail.com>
2021-04-01 09:22:28 -0400

    Add a method for getting chat color from color

--- src/main/java/io/github/mrsperry/mcutils/types/ColorTypes.java
+++ src/main/java/io/github/mrsperry/mcutils/types/ColorTypes.java
@@ -1,5 +1,6 @@
 package io.github.mrsperry.mcutils.types;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Color;
 
 import java.util.Map;
@@ -24,6 +25,25 @@ public class ColorTypes {
             Map.entry("WHITE", Color.WHITE),
             Map.entry("YELLOW", Color.YELLOW));
 
+    private static Map<Color, ChatColor> chatColors = Map.ofEntries(
+            Map.entry(Color.AQUA, ChatColor.AQUA),
+            Map.entry(Color.BLACK, ChatColor.BLACK),
+            Map.entry(Color.BLUE, ChatColor.BLUE),
+            Map.entry(Color.FUCHSIA, ChatColor.LIGHT_PURPLE),
+            Map.entry(Color.GRAY, ChatColor.DARK_GRAY),
+            Map.entry(Color.GREEN, ChatColor.DARK_GREEN),
+            Map.entry(Color.LIME, ChatColor.GREEN),
+            Map.entry(Color.MAROON, ChatColor.DARK_RED),
+            Map.entry(Color.NAVY, ChatColor.DARK_BLUE),
+            Map.entry(Color.OLIVE, ChatColor.DARK_GREEN),
+            Map.entry(Color.ORANGE, ChatColor.GOLD),
+            Map.entry(Color.PURPLE, ChatColor.DARK_PURPLE),
+            Map.entry(Color.RED, ChatColor.RED),
+            Map.entry(Color.SILVER, ChatColor.GRAY),
+            Map.entry(Color.TEAL, ChatColor.DARK_AQUA),
+            Map.entry(Color.WHITE, ChatColor.WHITE),
+            Map.entry(Color.YELLOW, ChatColor.YELLOW));
+
     public static Map<String, Color> getColors() {
         return ColorTypes.colors;
     }
@@ -40,5 +60,9 @@ public class ColorTypes {
         }
 
         return null;
+    }
+
+    public static ChatColor colorToChatColor(final Color color) {
+        return ColorTypes.chatColors.getOrDefault(color, null);
     }
 }
